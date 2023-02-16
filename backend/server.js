1116f9e19d174306359ee7b74b21b555b43e71ec
Juan11211 <avilesjuan628@gmail.com>
2023-02-16 17:23:12 -0500

    working on the backend

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,24 @@ const express = require('express')
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
-const { expressjwt } = require('express-jwt')
-
-
-mongoose.connect(
-    'mongodb://localhost:27017/booking-app',
-    () => console.log('Connected to the DB')
-  )
 
 app.use(express.json())
 
-app.use('/api/auth', require('./routes/authRouter.js'))
-app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256']})) // req.user
-app.use('/api/users', require('./routes/usersRouter.js'))
-app.use('/api/hotels', require('./routes/hotelsRouter.js'))
-app.use('/api/rooms', require('./routes/roomsRouter.js'))
- 
+const connect = async () => { 
+  try{
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MONGODB')
+  } catch (error){
+    throw(error)
+  }
+};
+
+
+app.use('/api/auth', require('./routes/authRouter'))
+app.use('/api/users', require('./routes/usersRouter'))
+app.use('/api/hotels', require('./routes/hotelsRouter'))
+app.use('/api/rooms', require('./routes/roomsRouter'))
+
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -31,5 +33,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(9000, () => {
+  connect()
     console.log(`Server is running on local port 9000`)
   }) 
